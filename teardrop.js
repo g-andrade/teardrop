48a02538483a792218e452e828a893f3c76824c2
Guilherme Andrade <g@gandrade.net>
2016-08-13 20:46:09 +0100

    Use WebGL instead of 2D canvas

--- teardrop.js
+++ teardrop.js
@@ -1,3 +1,4 @@
+/*jshint multistr: true */
 $(document).ready(init);
 
 // from https://gist.github.com/wteuber/6241786
@@ -9,19 +10,20 @@ var TEARDROP_M_VALUE = 0;
 var TEARDROP_ANGLE = 0;
 var TEARDROP_ANGLE_BASE_STEP = TWO_PI / 5;
 var MIN_TEARDROP_M_VALUE = 0;
-var MAX_TEARDROP_M_VALUE = 20;
+var MAX_TEARDROP_M_VALUE = 30;
 var TEARDROP_FUNCTION_PARAMS = [];
-var MAX_TEARDROP_FUNCTIONS = 40;
-var HARD_MAX_TEADROP_FUNCTIONS = 50;
+var MAX_TEARDROP_FUNCTIONS = 50;
+var HARD_MAX_TEADROP_FUNCTIONS = 80;
 
 var TEARDROP_SPACING;
-var TEARDROP_REL_SPACING = 0.02;
+var TEARDROP_REL_SPACING = 0.01;
 var BASE_TEARDROP_SPACING;
 var BASE_TEARDROP_REL_SPACING = 0.0;
 var BASE_TEARDROP_REL_SPACING_REL_GROWTH = 0.1;
 
 var BOARD_CANVAS;
 var BOARD_CANVAS_CTX;
+var SHADER_PROGRAM;
 
 var CURRENT_POSITION = {"x": 0, "y": 0};
 var MOUSE_POS = {"x": 0, "y": 0};
@@ -31,6 +33,24 @@ var TARGET_MAGNITUDE = 0.0;
 var TARGET_VISUAL_ANGLE = 0.0;
 var MAX_TARGET_APPROACH_REL_VEL_MAGNITUDE = 0.1; // relative units per second
 var MAX_TARGET_APPROACH_VEL_MAGNITUDE = {"x": 0, "y": 0}; // pixels per second
+
+// from: http://internetexception.com/post/2013/02/25/Line-in-WebGL-and-why-you-gonna-do-this-on-your-own.aspx
+var FRAG_SHADER_SOURCE = "\
+precision highp float;\
+uniform vec4 u_color;\
+void main(void) {\
+    gl_FragColor = u_color;\
+}";
+
+// from: http://internetexception.com/post/2013/02/25/Line-in-WebGL-and-why-you-gonna-do-this-on-your-own.aspx
+var VERTEX_SHADER_SOURCE = "\
+attribute vec3 a_position;\
+uniform vec4 u_color;\
+uniform mat4 u_mvMatrix;\
+uniform mat4 u_pMatrix;\
+void main(void) {\
+    gl_Position = u_pMatrix * u_mvMatrix * vec4(a_position, 1.0);\
+}";
 
 function init() {
     BOARD_CANVAS = $("#whiteboard")[0];
@@ -71,7 +91,7 @@ function init() {
 
                 functionParams = {
                     "countours": functionCountours,
-                    "strokeStyle": colorToColorStr(newColor())
+                    "color": newColor()
                 };
 
                 TEARDROP_FUNCTION_PARAMS.push(functionParams);
@@ -100,7 +120,7 @@ function init() {
                 if (MAX_TEARDROP_FUNCTIONS < HARD_MAX_TEADROP_FUNCTIONS)
                     MAX_TEARDROP_FUNCTIONS++;
             },
-            5000);
+            500);
 
     $("#whiteboard").bind("mousemove",
             function (e) {
@@ -116,10 +136,6 @@ function init() {
                 var touch = targetTouches[0];
                 MOUSE_POS = {"x": touch.pageX, "y": touch.pageY};
             });
-}
-
-function colorToColorStr(color) {
-    return '#' + ('000000' + color.toString(16)).slice(-6);
 }
 
 var COLOR_RANGE_LEN = 128;
@@ -152,13 +168,26 @@ function onResize(isFirst) {
     var boundingRect = BOARD_CANVAS.getBoundingClientRect();
     BOARD_CANVAS.width = boundingRect.width;
     BOARD_CANVAS.height = boundingRect.height;
-    BOARD_CANVAS_CTX = BOARD_CANVAS.getContext("2d");
+    BOARD_CANVAS_CTX =
+        BOARD_CANVAS.getContext("webgl", {antialias: true}) ||
+        BOARD_CANVAS.getContext("experimental-webgl", {antialias: true});
+    if (BOARD_CANVAS_CTX === undefined)
+        window.alert("no webgl support :(");
+
     TEARDROP_SPACING = Math.min(BOARD_CANVAS.width, BOARD_CANVAS.height) * TEARDROP_REL_SPACING;
     BASE_TEARDROP_SPACING = Math.min(BOARD_CANVAS.width, BOARD_CANVAS.height) * BASE_TEARDROP_REL_SPACING;
     MAX_TARGET_APPROACH_VEL_MAGNITUDE.x = MAX_TARGET_APPROACH_REL_VEL_MAGNITUDE * BOARD_CANVAS.width;
     MAX_TARGET_APPROACH_VEL_MAGNITUDE.y = MAX_TARGET_APPROACH_REL_VEL_MAGNITUDE * BOARD_CANVAS.height;
+    BOARD_CANVAS_CTX.viewport(0, 0, BOARD_CANVAS.width, BOARD_CANVAS.height);
+
     if (isFirst) {
         CURRENT_POSITION = {"x": BOARD_CANVAS.width / 2, "y": BOARD_CANVAS.height / 2};
+        BOARD_CANVAS_CTX.clearColor(0.0, 0.0, 0.0, 1.0);
+        BOARD_CANVAS_CTX.clearDepth(1.0);
+        BOARD_CANVAS_CTX.enable(BOARD_CANVAS_CTX.DEPTH_TEST);
+        BOARD_CANVAS_CTX.depthFunc(BOARD_CANVAS_CTX.LEQUAL);
+        initShaders();
+        initScene();
     }
     updatePosition();
     render();
@@ -193,14 +222,17 @@ function newTeardropFunction(m, teardropAngle) {
 
 function render() {
     refreshTeardropParams();
-    BOARD_CANVAS_CTX.clearRect(0, 0, BOARD_CANVAS.width, BOARD_CANVAS.height);
+    // clear screen
+    BOARD_CANVAS_CTX.clear(BOARD_CANVAS_CTX.COLOR_BUFFER_BIT | BOARD_CANVAS_CTX.DEPTH_BUFFER_BIT);
+
     var functionIndex, sliceIndex, angle, magnitude;
     var polarCoordinates, firstPolarCoordinates, lastPolarCoordinates;
-
-    var renderTotalSlices = 50;
+    var renderTotalSlices = 100;
     var renderXAccumulators = new Array(renderTotalSlices);
     var renderYAccumulators = new Array(renderTotalSlices);
     var prevFunctionPoints = [];
+    var pointSequences = [];
+
     for (var i = 0; i < renderTotalSlices; i++) {
         renderXAccumulators[i] = CURRENT_POSITION.x;
         renderYAccumulators[i] = CURRENT_POSITION.y;
@@ -209,8 +241,8 @@ function render() {
             functionIndex >= 0; functionIndex--)
     {
         functionParams = TEARDROP_FUNCTION_PARAMS[functionIndex];
-        functionCountours = functionParams.countours;
-        functionStrokeStyle = functionParams.strokeStyle;
+        var functionCountours = functionParams.countours;
+        var functionColor = functionParams.color;
         var functionPoints = [];
         for (sliceIndex = 0; sliceIndex < renderTotalSlices; sliceIndex++) {
             angle = ((sliceIndex / renderTotalSlices) * TWO_PI) - Math.PI;
@@ -227,38 +259,116 @@ function render() {
             var x = renderXAccumulators[sliceIndex];
             var y = renderYAccumulators[sliceIndex];
             functionPoints.push({"x":x, "y": y});
+        }
 
-            if (sliceIndex === 0) {
-                // first slice
-                BOARD_CANVAS_CTX.beginPath();
-                BOARD_CANVAS_CTX.moveTo(x, y);
-                continue;
-            }
-            BOARD_CANVAS_CTX.lineTo(x, y);
-            if (sliceIndex == (renderTotalSlices - 1)) {
-                // last slice
-                if (prevFunctionPoints.length > 0) {
-                    for (var prevFunctionPointsIndex = prevFunctionPoints.length - 1;
-                            prevFunctionPointsIndex >= 0;
-                            prevFunctionPointsIndex--)
-                    {
-                        var prevPoint = prevFunctionPoints[prevFunctionPointsIndex];
-                        BOARD_CANVAS_CTX.lineTo(prevPoint.x, prevPoint.y);
-                    }
-                    BOARD_CANVAS_CTX.lineTo(
-                            prevFunctionPoints[prevFunctionPoints.length - 1].x,
-                            prevFunctionPoints[prevFunctionPoints.length - 1].y);
-                    BOARD_CANVAS_CTX.lineTo(x, y);
-                }
-                BOARD_CANVAS_CTX.closePath();
-                BOARD_CANVAS_CTX.fillStyle = functionStrokeStyle;
-                BOARD_CANVAS_CTX.fill();
-                BOARD_CANVAS_CTX.strokeStyle = functionStrokeStyle;
-                BOARD_CANVAS_CTX.stroke();
-            }
+        var sectionPoints = functionPoints.concat([functionPoints[0]]);
+        if (prevFunctionPoints.length > 0) {
+            sectionPoints = sectionPoints
+                .concat(prevFunctionPoints)
+                .concat([prevFunctionPoints[0]]);
         }
         prevFunctionPoints = functionPoints;
+        pointSequences.push(
+                {"points": sectionPoints,
+                 "color": functionColor});
     }
+
+    for (functionIndex = pointSequences.length - 1;
+         functionIndex >= 0;
+         functionIndex--)
+    {
+        var pointSequence = pointSequences[functionIndex];
+        draw2Dpoints(pointSequence.points, pointSequence.color);
+    }
+}
+
+function draw2Dpoints(points, color) {
+    var canvasCenterX = BOARD_CANVAS.width / 2.0;
+    var canvasCenterY = BOARD_CANVAS.height / 2.0;
+    var vtx = new Float32Array(points.length * 3);
+    var idx = new Uint16Array(points.length);
+    for (var i = 0; i < points.length; i++) {
+        var point = points[i];
+        var x = (point.x - canvasCenterX) / canvasCenterX;
+        var y = (point.y - canvasCenterY) / canvasCenterY;
+        var baseVtxIndex = i * 3;
+        vtx[baseVtxIndex] = x;
+        vtx[baseVtxIndex + 1] = -y;
+        vtx[baseVtxIndex + 2] = 0.0; // z
+        idx[i] = i;
+    }
+
+    var color_r = ((color & 0xFF0000) >> 16) / 255.0;
+    var color_g = ((color & 0x00FF00) >> 8) / 255.0;
+    var color_b = ((color & 0x0000FF)) / 255.0;
+
+    initBuffers(vtx, idx);
+    BOARD_CANVAS_CTX.lineWidth(1.0);
+    BOARD_CANVAS_CTX.uniform4f(SHADER_PROGRAM.colorUniform, color_r, color_g, color_b, 1);
+    BOARD_CANVAS_CTX.drawElements(
+            BOARD_CANVAS_CTX.TRIANGLE_FAN,
+            points.length, BOARD_CANVAS_CTX.UNSIGNED_SHORT, 0);
+    unbindBuffers();
+}
+
+function initShaders() {
+    // from: http://internetexception.com/post/2013/02/25/Line-in-WebGL-and-why-you-gonna-do-this-on-your-own.aspx
+    var vertexShader = get_shader(BOARD_CANVAS_CTX.VERTEX_SHADER, VERTEX_SHADER_SOURCE);
+    var fragmentShader = get_shader(BOARD_CANVAS_CTX.FRAGMENT_SHADER, FRAG_SHADER_SOURCE);
+    SHADER_PROGRAM = BOARD_CANVAS_CTX.createProgram();
+    BOARD_CANVAS_CTX.attachShader(SHADER_PROGRAM, vertexShader);
+    BOARD_CANVAS_CTX.attachShader(SHADER_PROGRAM, fragmentShader);
+    BOARD_CANVAS_CTX.linkProgram(SHADER_PROGRAM);
+    BOARD_CANVAS_CTX.useProgram(SHADER_PROGRAM);
+    SHADER_PROGRAM.aposAttrib = BOARD_CANVAS_CTX.getAttribLocation(SHADER_PROGRAM, "a_position");
+    BOARD_CANVAS_CTX.enableVertexAttribArray(SHADER_PROGRAM.aposAttrib);
+    SHADER_PROGRAM.colorUniform = BOARD_CANVAS_CTX.getUniformLocation(SHADER_PROGRAM, "u_color");
+    SHADER_PROGRAM.pMUniform = BOARD_CANVAS_CTX.getUniformLocation(SHADER_PROGRAM, "u_pMatrix");
+    SHADER_PROGRAM.mvMUniform = BOARD_CANVAS_CTX.getUniformLocation(SHADER_PROGRAM, "u_mvMatrix");
+}
+
+function get_shader(type, source) {
+    var shader = BOARD_CANVAS_CTX.createShader(type);
+    BOARD_CANVAS_CTX.shaderSource(shader, source);
+    BOARD_CANVAS_CTX.compileShader(shader);
+    return shader;
+}
+
+function initBuffer(glELEMENT_ARRAY_BUFFER, data) {
+    // from: http://internetexception.com/post/2013/02/25/Line-in-WebGL-and-why-you-gonna-do-this-on-your-own.aspx
+    var buf = BOARD_CANVAS_CTX.createBuffer();
+    BOARD_CANVAS_CTX.bindBuffer(glELEMENT_ARRAY_BUFFER, buf);
+    BOARD_CANVAS_CTX.bufferData(glELEMENT_ARRAY_BUFFER, data, BOARD_CANVAS_CTX.STATIC_DRAW);
+    return buf;
+}
+
+function initBuffers(vtx, idx) {
+    vbuf = initBuffer(BOARD_CANVAS_CTX.ARRAY_BUFFER, vtx);
+    ibuf = initBuffer(BOARD_CANVAS_CTX.ELEMENT_ARRAY_BUFFER, idx);
+    BOARD_CANVAS_CTX.vertexAttribPointer(SHADER_PROGRAM.aposAttrib, 3, BOARD_CANVAS_CTX.FLOAT, false, 0, 0);
+}
+
+function initScene() {
+    BOARD_CANVAS_CTX.clearColor(0.0, 0.0, 0.0, 0.0);
+    mvMatrix =
+        [1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1];
+    pMatrix =
+        [1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1];
+    BOARD_CANVAS_CTX.enable(BOARD_CANVAS_CTX.DEPTH_TEST);
+    BOARD_CANVAS_CTX.clear(BOARD_CANVAS_CTX.COLOR_BUFFER_BIT | BOARD_CANVAS_CTX.DEPTH_BUFFER_BIT);
+    BOARD_CANVAS_CTX.uniformMatrix4fv(SHADER_PROGRAM.pMUniform, false, new Float32Array(pMatrix));
+    BOARD_CANVAS_CTX.uniformMatrix4fv(SHADER_PROGRAM.mvMUniform, false, new Float32Array(mvMatrix));
+}
+
+function unbindBuffers() {
+    BOARD_CANVAS_CTX.bindBuffer(BOARD_CANVAS_CTX.ARRAY_BUFFER, null);
+    BOARD_CANVAS_CTX.bindBuffer(BOARD_CANVAS_CTX.ELEMENT_ARRAY_BUFFER, null);
 }
 
 function interpolateMagnitudeFromContour(sortedContours, angle, leftIndex, rightIndex) {
